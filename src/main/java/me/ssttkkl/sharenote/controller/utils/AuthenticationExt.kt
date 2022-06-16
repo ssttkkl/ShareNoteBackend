03bdee80c9e84a25bb7a65c740e7cac0e90cfbb1
ssttkkl <huang.wen.long@hotmail.com>
2022-06-16 23:30:21 +0800

    在jwt中添加id的claim

--- src/main/java/me/ssttkkl/sharenote/controller/utils/AuthenticationExt.kt
+++ src/main/java/me/ssttkkl/sharenote/controller/utils/AuthenticationExt.kt
@@ -4,4 +4,4 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.jwt.Jwt
 
 val Authentication.userID: Int
-    get() = (this.principal as Jwt).subject.toInt()
+    get() = (this.principal as Jwt).getClaim("id")
